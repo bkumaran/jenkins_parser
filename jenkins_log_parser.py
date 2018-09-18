038f55762773575c9a19e260dfa54c14f119ac1b
Balakumaran G <balakumaran.gopal@couchbase.com>
2018-09-18 12:26:39 +0530

    Update jenkins_log_parser.py

--- jenkins_log_parser.py
+++ jenkins_log_parser.py
@@ -20,23 +20,29 @@ lines = urllib.urlopen(link).readlines()
 tc_count = 0
 for line in lines:
     line = line.strip('\n')
-    if "ERROR: " in line or "FAIL: " in line:
-	tc_count += 1
-        #array.append(str(tc_count)+"\t"+ line)
-	found = 1
+    if "ERROR: The system was unable to find the specified registry key or value." in line:
+	    pass
+    elif 'ERROR: The process "epmd.exe*" not found.' in line:
+	    pass
+    elif 'ERROR: The process "erl*" not found.' in line:
+	    pass
+    elif 'ERROR: The process "erl_crash.dump" not found.' in line:
+	    pass
+    elif "ERROR: " in line or "FAIL: " in line:
+	    tc_count += 1
+	    found = 1
     elif "Ran 1 test in" in line and found == 1 :
-	#array.append(line+"\n")
-	final_array.extend(array)
-	array =[]	
-	found = 0
+    	final_array.extend(array)
+    	array =[]
+    	found = 0
     elif "Ran 1 test in" in line and found == 0 :
-	tc_count += 1
+    	tc_count += 1
         string = str(tc_count)+"\t"+ "PASS"
         final_array.append(string)
     elif found == 1:
-	m = re.match(r"^(.*)(Exception|Error):\s+(.*)", line)
-	if m:
-            array.append(str(tc_count)+"\t"+ m.group(0))
+    	m = re.match(r"^(.*)(Exception|Error):\s+(.*)", line)
+    	if m:
+                array.append(str(tc_count)+"\t"+ m.group(0))
 
 for line in final_array:
     print line
